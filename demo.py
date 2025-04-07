3160866f8d36077cfc8a24e486bd4292f8735961
Diego Ivan Medina Martinez <dimm994@gmail.com>
2025-04-07 15:41:19 -0600

    clase finalizada

--- demo.py
+++ demo.py
@@ -1,8 +1,7 @@
 class Cuadrado:
-    def __init__(self,lado):
-    self.lado = lado
-             
-    def area (self):
-       return self.lado*self.lado
-Numlado = Cuadrado(0):
+    def __init__ (self,lado):
+self.lado = lado
+      def area (self):
+       return self.lado **2
+Numlado = Cuadrado(23):
 print ("el lado del cuadrado es",Numlado.area())
