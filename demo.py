31f3f0e81dda30c367e640204f7f7b93bb5c0403
Diego Ivan Medina Martinez <dimm994@gmail.com>
2025-04-07 15:29:45 -0600

    se utilizo init

--- demo.py
+++ demo.py
@@ -1,2 +1,8 @@
 class Cuadrado:
-    pass
+    def __init__(self,lado):
+    self.lado = lado
+             
+    def area (self):
+       return self.lado*self.lado
+Numlado = Cuadrado(0):
+print ("el lado del cuadrado es",Numlado.area())
